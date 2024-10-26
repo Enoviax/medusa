a0345c2cd91d909cdc3cccb94156dd5a945a2bec
lenox <lenoxmiheso@gmail.com>
2024-10-27 01:52:43 +0300

    fix

--- backend/src/admin/widgets/product-widget.tsx
+++ backend/src/admin/widgets/product-widget.tsx
@@ -6,7 +6,7 @@ const ProductWidget = () => {
     <Container className="divide-y p-0">
       <div className="flex items-center gap-14 px-6 py-4">
         <Heading level="h2">Product Preview/Social-share Link</Heading>
-        <Copy variant="default" content={`https://sharkeye.com`} className="w-8 h-8" />       
+        <Copy variant="default" content={`https://www.keensight.com`} className="w-8 h-8" />       
       </div>
     </Container>
   )
