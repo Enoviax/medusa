ef6061cdae9cb115e10c936f0c94f5e3b140b4fa
lenox <lenoxmiheso@gmail.com>
2024-11-02 08:11:35 +0300

    modified:   storefront/src/middleware.ts

--- storefront/src/middleware.ts
+++ storefront/src/middleware.ts
@@ -142,5 +142,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|favicon.ico|sitemap*.xml|sitemap-products.xml|sitemap-blog.xml|robots.txt).*)"],
+  matcher: ["/((?!api|_next/static|favicon.ico|sitemap*.xml|sitemap-0.xml|sitemap-blog.xml|robots.txt).*)"],
 }
